8f9db0cc039fc4d6d2ba26e93ca606ed4396cb49
Celest21 <zc.chetty@gmail.com>
2023-12-06 05:39:00 -0800

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,8 +5,12 @@ def best_score(a_dictionary):
     if a_dictionary is None or not a_dictionary:
         return None
 
-    max_score = max(a_dictionary.values())
+    max_score = float('-inf')
+    best_key = None
 
     for key, value in a_dictionary.items():
-        if value == max_score:
-            return key
+        if value > max_score:
+            max_score = value
+            best_key = key
+
+    return best_key
