433bf2046c295455a791c440b9d627ac4cd674d4
Celest21 <zc.chetty@gmail.com>
2023-12-06 04:01:36 -0800

    error fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
-    return(sum(set(my_list)))
+    # Use a set to keep track of unique integers
+    unique_set = set()
+
+    # Iterate over each element in the list
+    for num in my_list:
+        # Add the unique integers to the set
+        unique_set.add(num)
+
+    # Calculate the sum of unique integers
+    result = sum(unique_set)
+
+    return result
