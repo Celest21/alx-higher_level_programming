b59bc32fd95681b8dd1b834d2331ca44e68cf189
Celest21 <zc.chetty@gmail.com>
2024-03-27 15:43:49 -0700

    update

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Sends a JSON POST request to the provided URL with the contents of a file as the body of the request
-curl -s -X POST -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d "$(</dev/null)" "$1" < "$2"
 
