81f6f7985859b40a27ac1f32c11c3c9d197a981d
Celest21 <zc.chetty@gmail.com>
2024-03-27 15:15:35 -0700

    fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Sends a POST request to the provided URL with specified parameters and displays the body of the response
-curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
+# Sends a POST request to the provided URL with email and subject parameters and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
 
