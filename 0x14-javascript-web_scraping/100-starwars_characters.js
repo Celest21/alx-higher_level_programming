75a9daa4cbe9fda74e8f28a6246c9b4d57b4e5fd
Celest21 <zc.chetty@gmail.com>
2024-04-24 06:53:09 -0700

    advanced task

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,25 +1,35 @@
 #!/usr/bin/node
 
-const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const fetch = require('node-fetch');
+const process = require('process');
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
+async function getCharacters(movieId) {
+    try {
+        const response = await fetch(`https://swapi-api.alx-tools.com/films/${movieId}/`);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch movie data: ${response.status}`);
         }
-      });
+        const data = await response.json();
+        const characters = data.characters;
+        for (const characterUrl of characters) {
+            const characterResponse = await fetch(characterUrl);
+            if (!characterResponse.ok) {
+                throw new Error(`Failed to fetch character data: ${characterResponse.status}`);
+            }
+            const characterData = await characterResponse.json();
+            console.log(characterData.name);
+        }
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
     }
-  }
-});
+}
+
+if (process.argv.length < 3) {
+    console.error("Usage: node script.js <movie_id>");
+    process.exit(1);
+}
+
+const movieId = process.argv[2];
+getCharacters(movieId);
+
